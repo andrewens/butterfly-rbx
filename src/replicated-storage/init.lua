a0fdb064f3e0ba9a8fc6a59729151904e604b72a
andrewens <andrewens4@gmail.com>
2023-09-12 16:09:14 -0500

    Select a test to run with app state

--- src/replicated-storage/init.lua
+++ src/replicated-storage/init.lua
@@ -1,16 +1,46 @@
 local RunService = game:GetService("RunService")
 
---[[ SERVER ]]--
+--[[ SERVER ]]
+--
 if RunService:IsServer() then
-    error("Attempt to require butterfly on the server")
+	error("Attempt to require butterfly on the server")
 end
 
---[[ CLIENT ]]--
+--[[ CLIENT ]]
+--
+local ProxyTable = require(script:FindFirstChild("proxy-table"))
+
+-- state
+local State = ProxyTable({
+	selectionModal = false,
+	testIndex = 1,
+	numTests = 2,
+})
+
+function State.nextTest()
+	State.selectTest(State.testIndex + 1)
+end
+function State.prevTest()
+	State.selectTest(State.testIndex - 1)
+end
+function State.selectTest(newTestIndex)
+	assert(typeof(newTestIndex) == "number" and math.floor(newTestIndex) == newTestIndex)
+
+	if newTestIndex < 0 then  -- BEGIN
+		newTestIndex = 0
+	end
+	if newTestIndex > State.numTests then  -- END
+		newTestIndex = -1
+	end
+
+	State.testIndex = newTestIndex
+end
+
+-- public
 local butterfly = {}
 
-function butterfly.run(...)
-    print("Hello world")
+function butterfly.run()
+	
 end
 
 return butterfly
-
